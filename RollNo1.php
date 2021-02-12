f5d6c817fd8cd0ad5a078a228302ec1a9d95f304
sjatin050 <63118687+sjatin050@users.noreply.github.com>
2021-02-13 00:06:00 +0530

    Add files via upload

---
+++ RollNo1.php
@@ -0,0 +1,38 @@
+<?php
+$servername="localhost";
+$username="root";
+$password="";
+$dbname="college";
+$con=mysqli_connect($servername,$username,$password,$dbname);
+if (!$con)
+{echo"server not connected";}
+//else
+//{
+//echo"server is connected";
+//}
+if(!mysqli_select_db($con,$dbname))
+{
+echo"database not connected";
+}
+
+$query="SELECT rollno FROM student ";
+$result=mysqli_query($con,$query);
+
+?>
+
+<html>
+<head>
+</head>
+<body>
+<?php
+// <h1>ALL ROLL NOs</h1>
+echo "<h1>ALL ROLL NOs</h1>";
+while($row=mysqli_fetch_assoc($result))
+{
+  echo $row['rollno'];
+  echo "<br>";
+}
+
+?>
+</body>
+</html>
---
+++ assignment2.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+</head>
+<body>
+<h1> Assignment 2 </h1>
+<h2> Jatin Singh </h2>
+<h2> 191112033 </h2>
+<form  action="RollNO1.php" method="post">
+  <input type="submit" name="rollno" value="RollNO">
+</form>
+<br>
+<form  action="Name1.php" method="post">
+  <input type="submit" name="name" value="NAME">
+</form>
+ <br>
+<form  action="Age1.php" method="post">
+  <input type="submit" name="Age" value="AGE">
+</form>
+<br>
+<form  action="Score1.php" method="post">
+  <input type="submit" name="Score" value="SCORE">
+</form>
+<br>
+</body>
+</html>
